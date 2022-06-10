b812f16b7c2845a01fa941746b5533ca6772d8d6
maindishes <manynolan1@gmail.com>
2022-06-10 20:41:35 +0900

    Add: ex02 solve

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stirngREF = str;
+
+    std::cout << "stringPTR memory address: " << stringPTR << std::endl;
+    std::cout << "stringREF memory address: " << &stirngREF << std::endl;
+
+    std::cout << "value pointed to by stringPTR : " << *stringPTR << std::endl;
+    std::cout << "value pointed to by stringREF : " << stirngREF << std::endl;
+
+    return (0);
+}
