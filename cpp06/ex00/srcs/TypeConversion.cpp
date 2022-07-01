10dea621efde7f48a5691e8ef8bf996ceb47f4e4
maindishes <manynolan1@gmail.com>
2022-07-01 13:02:13 +0900

    Temp

--- cpp06/ex00/srcs/TypeConversion.cpp
+++ cpp06/ex00/srcs/TypeConversion.cpp
@@ -28,5 +28,155 @@ TypeConversion::TypeConversion(const std::string s)
 : _input(s)
 {
     _type = N;
-    if (_input.length() == 1 && !isdigit(_input.at(0)))
+    if (this->_input.length() == 1 && !isdigit(this->_input.at(0)))
+        this->_type = C;
 }
+
+int TypeConversion::toInt() const
+{
+    int temp;
+    if (this->_type == C)
+        temp = static_cast<int>(this->_input.at(0));
+    else
+    {
+        try
+        {
+            temp = std::stoi(_input);
+        }
+        catch(const std::exception& e)
+        {
+            throw(ImpossibelException());
+        }
+    }
+    return (temp);
+}
+
+char TypeConversion::toChar() const
+{
+    char temp;
+    if(this->_type == C)
+    {
+        try
+        {
+            if (this->_input.at(0) < 32 || this->_input.at(0) > 126) // 출력가능한 char range 32~126
+                throw NonDisplayException();
+            temp = this->_input.at(0);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    else
+    {
+        try
+        {
+            temp = static_cast<char>(std::stoi(this->_input));
+        }
+        catch(const std::exception& e)
+        {
+            throw ImpossibelException(); // 문자열
+        }
+        // 범위 넘어가는것들
+        if (temp < 32 || temp > 126)
+            throw NonDisplayException();
+    }
+    return (temp);
+}
+
+float TypeConversion::toFloat() const
+{
+    float temp;
+    if (_type == C)
+        temp = static_cast<float>(this->_input.at(0));
+    else
+    {
+        try
+        {
+            temp = std::stof(this->_input);
+        }
+        catch(const std::exception& e)
+        {
+            //문자열
+            throw (ImpossibelException());
+        }
+        
+    }
+    return (temp);
+}
+
+double TypeConversion::toDouble() const
+{
+    double ret;
+    if (_type == C)
+        ret = static_cast<double>(_input.at(0));
+    else
+    {
+        try
+        {
+            ret = static_cast<double>(std::stof(_input));
+        }
+        catch(const std::exception& e)
+        {
+            //문자열
+            throw (ImpossibelException());
+        }
+    }
+    return (ret);
+}
+
+const char * TypeConversion::NonDisplayException::what() const throw()
+{
+    return ("Non displayable");
+}
+
+const char * TypeConversion::ImpossibelException::what() const throw()
+{
+    return ("Impossible");
+}
+
+std::ostream &operator<<(std::ostream &os, TypeConversion const &s)
+{
+    try
+    {
+        os << "char : " << s.toChar() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        os << "int : " << s.toInt() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        os << "float : " << s.toFloat();
+        if (s.toFloat() - static_cast<int>(s.toFloat()) == 0)
+            os << ".0";
+        os << "f" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        os << "double : " << s.toDouble();
+        if (s.toDouble() - static_cast<int>(s.toDouble()) == 0)
+            os << ".0";
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return os;
+}
+
