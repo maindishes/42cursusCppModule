ed00aef06275ceebac82be495e0a4d37d0d78194
maindishes <manynolan1@gmail.com>
2022-07-13 16:59:30 +0900

    Temp

--- cpp08/ex01/incs/Span.hpp
+++ cpp08/ex01/incs/Span.hpp
@@ -3,6 +3,31 @@
 
 #include <exception>
 #include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Span
+{
+    private:
+        std::vector<int> v;
+        // unsinged int _N;
+    public:
+        Span();
+        Span(unsigned int n);
+        Span(const Span &rhs);
+        
+        Span &operator=(const Span &rhs);
+        ~Span();
+
+        void addNumber(int num);
+        void addNumber(std::vector<int>::iterator const &begin, std::vector<int>::iterator const &end);
+
+        int shortestSpan(void) const;
+        int longestSpan(void) const;
+
+
+
+}
 
 class OutOfRangeException : public std::exception {
 public:
