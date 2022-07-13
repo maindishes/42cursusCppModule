c3d67d7c2cefbe433a2152e559ddcbb5ee155d46
maindishes <manynolan1@gmail.com>
2022-07-13 15:58:39 +0900

    Add : easyfind , iterator ,find

---
+++ cpp08/ex00/incs/easyfind.hpp
@@ -0,0 +1,22 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <exception>
+#include <algorithm>
+
+class OutOfRangeException : public std::exception {
+public:
+	const char* what() const throw() {
+		return ("OutOfRangeException");
+	}
+};
+template <typename T>
+typename T::iterator easyfind(T &container, int target)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), target);
+    if (it == container.end())
+        throw OutOfRangeException();
+    return it;
+}
+
+#endif
---
+++ cpp08/ex00/srcs/main.cpp
@@ -0,0 +1,99 @@
+#include "../incs/easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+
+
+void vectorTest() 
+{
+	std::cout << "[vector Test]"  << std::endl;
+	std::vector<int> vec;
+	for (int i = 0; i < 10; i++) 
+    {
+		vec.push_back(i);
+	}
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) 
+    {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	try 
+    {
+		std::vector<int>::iterator p1 = easyfind(vec, 2);
+		std::cout << *p1 << std::endl;
+
+		std::vector<int>::iterator p2 = easyfind(vec, 10);
+		std::cout << *p2 << std::endl;
+	} catch (std::exception &e) 
+    {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+}
+
+void dequeTest() 
+{
+	std::cout << "[deque Test]" << std::endl;
+	std::deque<int> dq;
+	for (int i = 0; i < 10; i++) 
+    {
+		dq.push_back(i);
+	}
+	for (std::deque<int>::iterator it = dq.begin(); it != dq.end(); ++it) 
+    {
+		std::cout  << *it << " ";
+	}
+	std::cout << std::endl;
+
+	try 
+    {
+		std::deque<int>::iterator p3 = easyfind(dq, 2);
+		std::cout << *p3 << std::endl;
+
+		std::deque<int>::iterator p4 = easyfind(dq, 10);
+		std::cout << *p4 << std::endl;
+	} catch (std::exception &e) 
+    {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void listTest() 
+{
+	std::cout << "[list Test]"  << std::endl;
+	std::list<int> lst;
+	for (int i = 0; i < 10; i++) 
+    {
+		lst.push_back(i);
+	}
+	for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) 
+    {
+		std::cout  << *it << " ";
+	}
+	std::cout  << std::endl;
+
+	try 
+    {
+		std::list<int>::iterator p5 = easyfind(lst, 2);
+		std::cout << *p5 << std::endl;
+
+		std::list<int>::iterator p6 = easyfind(lst, 10);
+		std::cout << *p6 << std::endl;
+	} catch (std::exception &e) 
+    {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int main(void)
+{
+	vectorTest();
+	dequeTest();
+	listTest();
+	return (0);
+}
